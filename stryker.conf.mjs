cc57935f9c9e3b05274816959533444a2f96a322
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-06-22 13:58:52 +0200

    feat: :wrench: add stryker & vitest config

---
+++ stryker.conf.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+  _comment:
+    "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
+  packageManager: "pnpm",
+  reporters: ["html", "clear-text", "progress"],
+  testRunner: "vitest",
+  vitest: {
+    configFile: "vitest.config.ts",
+  },
+  checkers: ["typescript"],
+  tsconfigFile: "tsconfig.json",
+  typescriptChecker: {
+    prioritizePerformanceOverAccuracy: true,
+  },
+  logLevel: "error",
+  plugins: [
+    "@stryker-mutator/vitest-runner",
+    "@stryker-mutator/typescript-checker",
+  ],
+};
+export default config;
